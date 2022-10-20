ecc149ce98c4d1af81116f87b05fb66fbe52332c
tomthethird <tflopez3@gmail.com>
2022-10-20 09:05:32 +0800

    added javascript functions

--- client/src/components/BestSellers.jsx
+++ client/src/components/BestSellers.jsx
@@ -5,51 +5,63 @@ const BestSellers = () => {
 
     const getProducts = async () => {
         try {
-          const response = await fetch(`http://localhost:8000/api/get`, {
-            method: 'GET',
-            headers: { 'accept': 'application/json'}
-          });
-          const parseData = await response.json();
-          console.log(parseData)
-          setProduct(parseData)
+            const response = await fetch(`http://localhost:8000/api/get`, {
+                method: 'GET',
+                headers: { 'accept': 'application/json' }
+            });
+            const parseData = await response.json();
+            console.log(parseData)
+            setProduct(parseData)
 
         } catch (error) {
-          console.error(error)
+            console.error(error)
         }
-      }
-    
+    }
+
+    const discountRate = (a, b) => {
+        const rate = ((b-a)/b)*100
+        return Math.round(rate)
+    }
+
     useEffect(() => {
         getProducts()
     }, []);
 
     return (
         <div>
-            <div className="bg-secondary-wash pt-5 py-5">
-                <div className="row w-75 mx-auto">
-                    <div className="col-xl-12 text-center pb-5">
-                        <h1 className="fw-bold text-primary-light">Best Sellers</h1>
-                    </div>
-                </div>
-
-                <div className=" container">
-                    <div className="row g-3">
-                        <div className="col-md-8 col-xl-12 d-flex justify-content-center m-0 flex-wrap position-relative">
-                        
-                        {product.map((product) => {
-                            return <div className="bg-white shadow rounded-5 text-center p-4 m-3">
-                                <img src={`http://localhost:8000/${product.photo}`} alt="cocojam" className="productImage" />
-                                <h5 className="text-primary-light mb-0">{product.product}</h5>
-                                <p className="mb-2">{product.brand}</p>
-                                <h3 className="text-primary fw-bold">P {product.price}</h3>
-                            </div>
-                        })}
-                        
+            <div className="bg-secondary-wash pt-5 py-5 ">
+                <div className="row main-container">
+                    <div className="row w-75 mx-auto">
+                        <div className="col-xl-12 text-center pb-5">
+                            <h1 className="fw-bold text-primary-light">Best Sellers</h1>
                         </div>
                     </div>
-                </div>
-                <div className="row w-75 mx-auto">
-                    <div className="col-xl-12 text-center pt-5">
-                        <button className="btn btn-primary rounded-pill text-white mt-4 semibold fs-5 py-2 px-5">Discover All Best Sellers</button>
+                    <div className="col-xl-12 container p-0">
+                        <div className="d-flex flex-md-nowrap flex-wrap pb-4 justify-content-center">
+                            <div className="row justify-content-center m-0 p-0">
+                                <div className="col-xl-10 d-flex flex-wrap flex-md-nowrap position-relative m-0 p-0">
+
+                                    {product.map((product) => {
+                                        return <div className="bg-white snip-card shadow rounded-5 p-5 m-3 align-items-end">
+                                            <div>
+                                                {product.sale ? (<span class="sale badge px-3 py-2 rounded-pill bg-danger">{discountRate(product.discountPrice,product.price)}% OFF</span>) : (<></>)} 
+                                                <img src={`${product.photo}`} alt="cocojam" className="productImage" />
+                                            </div>
+                                            <h5 className="text-primary-light mb-0">{product.product}</h5>
+                                            <p className="mb-2">{product.brand}</p>
+                                            {product.sale ? (<h3><strong className="text-primary fw-bold">P {product.discountPrice}</strong> <em className="text-decoration-line-through text-muted small">P {product.price}</em></h3>)
+                                            : (<h3 className="text-primary fw-bold">P {product.price}</h3>)}
+                                        </div>
+                                    })}
+
+                                </div>
+                            </div>
+                        </div>
+                        <div className="row w-75 mx-auto">
+                            <div className="col-xl-12 text-center pt-5">
+                                <button className="btn btn-primary rounded-pill text-white mt-4 semibold fs-5 py-2 px-5">Discover All Best Sellers</button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
